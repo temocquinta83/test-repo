f4a79778e944d74ab0e6b91eb1ecf74c87c383a7
Temoc Salas <temoc.salas@movilitas.com>
2019-08-02 02:03:26 -0500

    app bugs

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+//const cors = require('cors');
+//const bodyParser = require('body-parser');
+//const morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('tiny'));
-app.use(cors);
-app.use(bodyParser.json());
+//app.use(morgan('tiny'));
+//app.use(cors);
+//app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.json({
-        "HELLO": "WORLD"
-    });
+    res.send("hello world");
 });
 
 const port = process.env.PORT || 4000;
